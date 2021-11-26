7db45f653e505f4311282b9a96a477b2adb744df
thainamariani <marianithaina@gmail.com>
2021-11-26 17:15:06 -0300

    aula 4

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,10 +1,10 @@
 const Doctor = require("../models/Doctor")
 
 const createDoctor = async (req, res) => {
-    const { name, crm, specialty, clinic, phone, favorite } = req.body
+    const { name, crm, speciality, clinic, phone, favorite } = req.body
     try{
-        const doctor = await Doctor.create({name, crm, specialty, clinic, phone, favorite})
-        console.log("Medico ${doctor.name} criado")
+        const doctor = await Doctor.create({name, crm, speciality, clinic, phone, favorite})
+        console.log(`Medico ${doctor.name} criado`)
         res.status(201).send(doctor)
     } catch (error) {
         res.status(500).send({ message: error.message })
@@ -26,7 +26,42 @@ const getAllDoctors = async (req, res) => {
     }
 }
 
+const getDoctor = async (req, res) => {
+    const doctorId = req.params.id
+    try {
+        const doctor = await Doctor.findOne({
+            where: { id: doctorId}
+        })
+        if (doctor) {
+            res.status(200).send(doctor)
+        } else{
+            res.status(404).send({ message: `Médico não encontrado com o id ${doctorId}`})
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
+const updateDoctor = async (req, res) => {
+    const doctorId = req.params.id
+    const {name, crm, speciality, clinic, phone, favorite } = req.body
+    try{
+        const rowsUpdated = await Doctor.update ({ name, crm, speciality, clinic, phone, favorite }, {
+            where: { id: doctorId }
+        })
+        if (rowsUpdated && rowsUpdated > 0){
+            res.status(200).send({ message: `${rowsUpdated[0]} medicos(s) atualizado(s)` })
+        } else{
+            res.status(404).send({ message: `Médico com id ${doctorId} não encontrado para atualizar` })
+        }
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
 module.exports = {
     createDoctor,
-    getAllDoctors
+    getAllDoctors,
+    getDoctor,
+    updateDoctor
 }
